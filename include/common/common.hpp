4912ed51d5e60f93810e09d7452ed72dc0ce2007
Fabii137 <krsticfabian@gmail.com>
2025-10-05 14:32:40 +0200

    added trim to common and started adding encryption/decryption

--- include/common/common.hpp
+++ include/common/common.hpp
@@ -1,9 +1,14 @@
 #pragma once
+#include <string>
 
 class Common {
 public:
     Common();
     ~Common();
+
+    static void trim(std::string& s);
+    static std::string encrypt(const std::string& s);
+    static std::string decrypt(const std::string& s);
 private:
 
 };
--- src/common/common.cpp
+++ src/common/common.cpp
@@ -1,5 +1,4 @@
-#include <print>
-#include <common/common.hpp>
+#include "common/common.hpp"
 
 Common::Common() {
 
@@ -8,4 +7,28 @@ Common::Common() {
 Common::~Common() {
 
 }
+
+void Common::trim(std::string &s) {
+    const size_t start = s.find_first_not_of(" \t\n\r");
+    const size_t end = s.find_last_not_of(" \t\n\r");
+
+    if (start == std::string::npos) {
+        s.clear();
+    } else {
+        s = s.substr(start, end - start + 1);
+    }
+}
+
+std::string Common::encrypt(const std::string& s) {
+    std::string result;
+    for (size_t i = 0; i < s.length(); i++) {
+    //    result +=
+    }
+    return result;
+}
+
+std::string Common::decrypt(const std::string& s) {
+
+}
+
 
